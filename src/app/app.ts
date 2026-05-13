11607698b9368db627e078db41b8e526f603ec28
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2026-05-13 18:10:31 +0200

    feat: add empty discover page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Discover } from "./pages/discover/discover";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Discover],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
