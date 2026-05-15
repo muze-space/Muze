25eb4f0719dd47e93accdccc024df2cd23698cb1
vladaworkflow-ops <vlada.work.flow@gmail.com>
2026-05-15 10:34:42 +0200

    feat: create footer component, add style

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "../../components/header/header";
+import { Footer } from '../../footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
