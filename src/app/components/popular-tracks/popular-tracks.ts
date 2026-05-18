dd137e7d0a87c39df6cc7daaddf98888377c82fe
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2026-05-18 14:53:07 +0200

    feat: add empty popular-tracks component

---
+++ src/app/components/popular-tracks/popular-tracks.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popular-tracks',
+  imports: [],
+  templateUrl: './popular-tracks.html',
+  styleUrl: './popular-tracks.css',
+})
+export class PopularTracks {}
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from "../../components/header/header";
 import { Footer } from '../../footer/footer';
+import { PopularTracks } from "../../components/popular-tracks/popular-tracks";
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Footer],
+  imports: [Header, Footer, PopularTracks],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
