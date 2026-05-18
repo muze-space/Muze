16f08c073f91f959c08334ca9977a870e993831b
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2026-05-18 16:03:45 +0200

    feat: get tracks from API, setup simple popular-tracks ui

--- src/app/components/popular-tracks/popular-tracks.ts
+++ src/app/components/popular-tracks/popular-tracks.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { JamendoTrack, JamendoTracksResponse } from '../../model/track.model';
+import { API_CONFIG } from '../../config';
 
 @Component({
   selector: 'app-popular-tracks',
@@ -6,4 +9,18 @@ import { Component } from '@angular/core';
   templateUrl: './popular-tracks.html',
   styleUrl: './popular-tracks.css',
 })
-export class PopularTracks {}
+export class PopularTracks implements OnInit {
+  private httpClient = inject(HttpClient);
+  tracks = signal<JamendoTrack[]>([]);
+
+  ngOnInit() {
+    this.httpClient.get<JamendoTracksResponse>(`${API_CONFIG.baseUrl}/tracks/`, {
+      params: {
+        client_id: API_CONFIG.clientId,
+        format: 'jsonpretty',
+        limit: '10',
+        order: 'popularity_total',
+      },
+    }).subscribe(response => this.tracks.set(response.results));
+  }
+}
