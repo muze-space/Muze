60fd183c5015900b304e9a32df81efc509b63eca
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2026-06-01 16:16:28 +0200

    feat: add styles to track-item

--- src/app/shared/components/track-item/track-item.ts
+++ src/app/shared/components/track-item/track-item.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Track } from '../../../core/models/track.model';
 import { DurationPipe } from '../../pipes/duration.pipe';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-track-item',
-  imports: [DurationPipe],
+  imports: [DurationPipe, DatePipe],
   templateUrl: './track-item.html',
   styleUrl: './track-item.css',
 })
