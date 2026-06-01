4882f8dbfb6ca1e4fc4414e5d3894ba3ce5f302e
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2026-06-01 18:48:25 +0200

    feat: implement genre click navigation and corresponding search functionality

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,7 +1,9 @@
 import { TRACK_GENRES } from '../../core/constants/genre.const';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Tracks } from '../../shared/components/tracks/tracks';
 import { TrackOrder } from '../../core/enums/track-order.enum';
+import { Router } from '@angular/router';
+import { AppRoutes } from '../../core/enums/app-routes.enum';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +12,14 @@ import { TrackOrder } from '../../core/enums/track-order.enum';
   styleUrl: './home.css',
 })
 export class Home {
+  private router = inject(Router);
+
   readonly trackOrder = TrackOrder;
   readonly genres = TRACK_GENRES;
+
+  onGenreClick(value: string) {
+    this.router.navigate([AppRoutes.Search], {
+      queryParams: { genre: value },
+    });
+  }
 }
--- src/app/pages/search/search.ts
+++ src/app/pages/search/search.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Track } from '../../core/models/track.model';
+import { TracksService } from '../../core/services/tracks.service';
+import { TRACK_GENRES } from '../../core/constants/genre.const';
+import { TrackOrder } from '../../core/enums/track-order.enum';
+import { SEARCH_PARAMS } from '../../core/constants/search-params.const';
 
 @Component({
   selector: 'app-search',
@@ -6,4 +12,21 @@ import { Component } from '@angular/core';
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
-export class Search {}
+export class Search implements OnInit {
+  private route = inject(ActivatedRoute);
+  private trackService = inject(TracksService);
+
+  tracks = signal<Track[]>([]);
+
+  ngOnInit() {
+    this.route.queryParams.subscribe((params) => {
+      const genre = TRACK_GENRES.find((g) => g.value === params[SEARCH_PARAMS.genre]);
+
+      if (params[SEARCH_PARAMS.genre]) {
+        this.trackService
+          .getTracks(TrackOrder.PopularityTotal, genre)
+          .subscribe((response) => this.tracks.set(response.results));
+      }
+    });
+  }
+}
