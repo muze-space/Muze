c2e5dd636dac32a5de9b63dad30addc09f7d7396
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2026-06-08 14:01:21 +0300

    fix: implement navigation to home in NotFound component

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppRoutes } from '../../core/enums/app-routes.enum';
 
 @Component({
   selector: 'app-not-found',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
   templateUrl: './not-found.html',
   styleUrl: './not-found.css',
 })
-export class NotFound {}
+export class NotFound {
+  private readonly _router = inject(Router);
+
+  goHome(): void {
+    this._router.navigate([AppRoutes.Home]).then(r => r);
+  }
+}
