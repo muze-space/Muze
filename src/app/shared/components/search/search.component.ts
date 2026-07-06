b8ab3cadd0eb4596b5d15200828e92ddbe253a90
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2026-07-06 13:28:34 +0200

    refactor: move search form subscription to constructor

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -26,38 +26,40 @@ export class SearchComponent {
 
   private _tracksService = inject(TracksService);
 
-  private _subscription = this.searchForm.controls.query.valueChanges
-    .pipe(
-      map((value) => value.trim()),
-      debounceTime(500),
-      distinctUntilChanged(),
-      takeUntilDestroyed(),
-    )
-    .subscribe((query) => {
-      if (query) {
-        this.isLoading.set(true);
-        this.error.set(null);
+  constructor() {
+    this.searchForm.controls.query.valueChanges
+      .pipe(
+        map((value) => value.trim()),
+        debounceTime(500),
+        distinctUntilChanged(),
+        takeUntilDestroyed(),
+      )
+      .subscribe((query) => {
+        if (query) {
+          this.isLoading.set(true);
+          this.error.set(null);
 
-        this._tracksService
-          .getTracks({ search: query, order: TrackOrder.Relevance })
-          .pipe(finalize(() => this.isLoading.set(false)))
-          .subscribe({
-            next: (response) => {
-              this.suggestions.set(response.results);
-              this.isResultsWindowOpen.set(true);
-            },
-            error: (err) => {
-              this.error.set(err);
-              this.isResultsWindowOpen.set(false);
-            },
-          });
-      } else {
-        this.suggestions.set([]);
-        this.isResultsWindowOpen.set(false);
-        this.isLoading.set(false);
-        this.error.set(null);
-      }
-    });
+          this._tracksService
+            .getTracks({ search: query, order: TrackOrder.Relevance })
+            .pipe(finalize(() => this.isLoading.set(false)))
+            .subscribe({
+              next: (response) => {
+                this.suggestions.set(response.results);
+                this.isResultsWindowOpen.set(true);
+              },
+              error: (err) => {
+                this.error.set(err);
+                this.isResultsWindowOpen.set(false);
+              },
+            });
+        } else {
+          this.suggestions.set([]);
+          this.isResultsWindowOpen.set(false);
+          this.isLoading.set(false);
+          this.error.set(null);
+        }
+      });
+  }
 
   closeResults(): void {
     this.isResultsWindowOpen.set(false);
