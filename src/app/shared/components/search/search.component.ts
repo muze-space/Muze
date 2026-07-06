93b7aef146c918e1eb8a5d8821e3bb6105e0a56a
Ihar Leshchanka <ihar.leshchanka@gmail.com>
2026-07-06 12:33:29 +0200

    feat: implement reactive form for search input

--- src/app/shared/components/search/search.component.ts
+++ src/app/shared/components/search/search.component.ts
@@ -1,14 +1,16 @@
 import { Component, inject, signal } from '@angular/core';
-import { debounceTime, finalize, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, finalize, map } from 'rxjs';
 import { TracksService } from '../../../core/services/tracks.service';
 import { TrackOrder } from '../../../core/enums/track-order.enum';
 import { Track } from '../../../core/models/track.model';
 import { TrackItem } from '../track-item/track-item';
 import { ClickOutsideDirective } from '../../directives/click-outside.directive';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-search',
-  imports: [TrackItem, ClickOutsideDirective],
+  imports: [TrackItem, ClickOutsideDirective, ReactiveFormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
@@ -17,44 +19,45 @@ export class SearchComponent {
   isResultsWindowOpen = signal<boolean>(false);
   isLoading = signal<boolean>(false);
   error = signal<string | null>(null);
-  query = '';
 
-  private _tracksService = inject(TracksService);
-  private _searchSubject = new Subject<string>();
-
-  private _subscription = this._searchSubject.pipe(debounceTime(500)).subscribe((searchQuery) => {
-    if (searchQuery.trim()) {
-      this.isLoading.set(true);
-      this.error.set(null);
-
-      this._tracksService
-        .getTracks({ search: searchQuery, order: TrackOrder.Relevance })
-        .pipe(finalize(() => this.isLoading.set(false)))
-        .subscribe({
-          next: (response) => {
-            this.suggestions.set(response.results);
-            this.isResultsWindowOpen.set(true);
-            console.log('_subscription: ', this.query);
-          },
-          error: (err) => {
-            this.error.set(err);
-            this.isResultsWindowOpen.set(false);
-          },
-        });
-    } else {
-      this.suggestions.set([]);
-      this.isResultsWindowOpen.set(false);
-      this.isLoading.set(false);
-      this.error.set(null);
-    }
+  searchForm = new FormGroup({
+    query: new FormControl('', { nonNullable: true }),
   });
 
-  onInput(event: Event) {
-    this.query = (event.target as HTMLInputElement).value;
-    console.log('onInput: ', this.query);
+  private _tracksService = inject(TracksService);
 
-    this._searchSubject.next(this.query);
-  }
+  private _subscription = this.searchForm.controls.query.valueChanges
+    .pipe(
+      map((value) => value.trim()),
+      debounceTime(500),
+      distinctUntilChanged(),
+      takeUntilDestroyed(),
+    )
+    .subscribe((query) => {
+      if (query) {
+        this.isLoading.set(true);
+        this.error.set(null);
+
+        this._tracksService
+          .getTracks({ search: query, order: TrackOrder.Relevance })
+          .pipe(finalize(() => this.isLoading.set(false)))
+          .subscribe({
+            next: (response) => {
+              this.suggestions.set(response.results);
+              this.isResultsWindowOpen.set(true);
+            },
+            error: (err) => {
+              this.error.set(err);
+              this.isResultsWindowOpen.set(false);
+            },
+          });
+      } else {
+        this.suggestions.set([]);
+        this.isResultsWindowOpen.set(false);
+        this.isLoading.set(false);
+        this.error.set(null);
+      }
+    });
 
   closeResults(): void {
     this.isResultsWindowOpen.set(false);
